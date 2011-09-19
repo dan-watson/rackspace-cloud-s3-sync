dd2dd48053a26dab3d41788efed6441a28fa3d8b
Daniel Watson <dan@dotnetguy.co.uk>
2011-09-19 16:56:16 +0100

    Fixed bug in rake task

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,7 @@
 namespace :db do
   desc "Create database"
   task :migrate do
-    exec 'sequel -m db/migrations/. sqlite://' + File.dirname(__FILE__) + '/db/rackspace-cloud-s3.db'
+    exec 'bundle exec sequel -m db/migrations/. sqlite://' + File.dirname(__FILE__) + '/db/rackspace-cloud-s3.db'
   end
 
   desc "Delete Database File"
